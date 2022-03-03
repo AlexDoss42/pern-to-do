57e7ce8624af3df4cd175cb9755283ff1c63d215
AlexDoss42 <alex.doss42@gmail.com>
2022-03-03 13:35:49 -0700

    connects database to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,28 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//ROUTES//
+
+//create a todo
+
+app.post("/todos", async(req, res) => {
+    
+})
+
+//get all todos
+
+//get a todo
+
+//edit a todo
+
+//delete a todo
 
 app.listen(5000, () => {
     console.log('ITS WORKING!!!! IIIITTTTTSSSS WORKING!!!! on port 5000')
