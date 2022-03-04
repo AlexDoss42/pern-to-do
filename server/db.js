c524643abccc5443a302f15516d2d58e067a75fa
AlexDoss42 <alex.doss42@gmail.com>
2022-03-03 19:24:02 -0700

    finally has the database working and set up with a todo table

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "adoss",
+    user: "postgres",
     password: process.env.DBPW,
     host: "localhost",
-    port: 5433,
-    database: "adoss"
+    port: 5432,
+    database: "perntodo"
 });
 
 module.exports = pool;
